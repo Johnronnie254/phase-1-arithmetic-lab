e85989636465645ee6d6a897dcd40b9f2981266f
Ochieng John Ronnie <johnronnie18@gmail.com>
2023-09-21 12:55:09 +0300

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 
 // Write your code here
+const num1= 31;
+const num2 = 2;
+function multiply (num1,num2){
+    multiply = num1*num2;
+    return multiply;
+}
+console.log(multiply(num1,num2))
+
+//task 2
+function random (a){
+    random = Math.ceil(Math.random())
+    return random;
+}
+console.log(random())
+//task 3
+
+const num3= 24;
+const num4 = 5;
+function mod (num3,num4){
+    mod =  num3%num4;
+    return mod;
+}
+console.log(mod(num3,num4))
+
+//task 4
+
+function max (){
+    max= Math.max(2,7,6,9,20)
+    return max;
+}
+console.log(max());
